07ff0be655845cde34d60f2ecba5f084c068f48a
Sarin Yongsuwan <yongsuwan.sb@gmail.com>
2023-04-11 21:19:42 +0700

    Modity Entity(not complete)

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,23 +1,73 @@
 package entities;
 
+import java.util.ArrayList;
+
+import buildings.Node;
+import logic.Player;
+import material.Material;
 import type.EntityType;
 
 public class Entity implements Moveable {
 	private EntityType type;
-	private Point position;
+	private boolean isMove;
+	private int duration;
+	private Node position;
+	private Player owner;
+	private ArrayList<Material> wantMaterials;
 	
-	public Entity(EntityType type) {}
+	public Entity(EntityType type, Player owner, int duration, ArrayList<Material> wantMaterials) {
+		
+	}
 
 	@Override
-	public boolean canMove() {
+	public boolean canMove(Node node) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean moveTo(Point p) {
+	public void moveTo(Node node) {
 		// TODO Auto-generated method stub
-		return false;
+		
 	}
 
+	public EntityType getType() {
+		return type;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = Math.max(0, duration);
+	}
+
+	public Node getPosition() {
+		return position;
+	}
+
+	public void setPosition(Node position) {
+		this.position = position;
+	}
+
+	public Player getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Player owner) {
+		this.owner = owner;
+	}
+
+	public ArrayList<Material> getWantMaterials() {
+		return wantMaterials;
+	}
+
+	public void setWantMaterials(ArrayList<Material> wantMaterials) {
+		this.wantMaterials = wantMaterials;
+	}
+
+
+
+	
 }
