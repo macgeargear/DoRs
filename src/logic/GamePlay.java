c58018bf436bd32c89a97850701e306b72678385
Sarin Yongsuwan <yongsuwan.sb@gmail.com>
2023-04-17 16:51:39 +0700

    Init Class GamePlay

--- src/logic/GamePlay.java
+++ src/logic/GamePlay.java
@@ -4,33 +4,124 @@ import java.util.ArrayList;
 
 
 import buildings.Building;
+import buildings.Edge;
+import buildings.Node;
 import card.EffectCard;
+import entities.Entity;
 import material.Map;
 
 public class GamePlay {
 	public static GamePlay instance;
+	private int roundAmount;
+	private int currentRound;
+	private int currentPlayer;
+	private Marketplace marketplace;
 	private ArrayList<Player> allPlayers;
-	private ArrayList<Building> allBuilding;
+	private ArrayList<Node> allNodes;
+	private ArrayList<Edge> allEdges;
 	private ArrayList<Map> allMaps;
 	private ArrayList<EffectCard> cardInDeck;
-	private int currentPlayer;
+	private ArrayList<Entity> allEntities;
 	
-	GamePlay() {}
-	
-	public void drawCard() {}
-	public void goToNextPlayer() {}
-	public int rollDice() {
-		return 1 + (int)(Math.random() * 6);
+	public GamePlay() {
+		this.roundAmount = 10;
+		this.currentRound = 0;
+		this.currentPlayer = 0;
+		this.marketplace = new Marketplace();
+		this.allPlayers = new ArrayList<Player>();
+		this.allNodes = new ArrayList<Node>();
+		this.allEdges = new ArrayList<Edge>();
+		this.allMaps = new ArrayList<Map>();
+		this.cardInDeck = new ArrayList<EffectCard>();
+		this.allEntities = new ArrayList<Entity>();
 	}
+	
+	public GamePlay(int roundAmount) {
+		this.roundAmount = roundAmount;
+		this.currentRound = 0;
+		this.currentPlayer = 0;
+		this.marketplace = new Marketplace();
+		this.allPlayers = new ArrayList<Player>();
+		this.allNodes = new ArrayList<Node>();
+		this.allEdges = new ArrayList<Edge>();
+		this.allMaps = new ArrayList<Map>();
+		this.cardInDeck = new ArrayList<EffectCard>();
+		this.allEntities = new ArrayList<Entity>();
+	}
+	
+	public void draw() {}
+	
+	public void goToNextPlayer() {}
+	
+	public void rollDice() {}
+	
 	public void trade(Player p1, Player p2) {}
-	public void addBuilding(Building newBuilding) {}
+	
+	public void addEntity(Entity entity) {}
+	
 	public void addPlayer(Player player) {}
 	
-	public static GamePlay getInstance() {}
-	public ArrayList<Player> getResult() {}
-	public int getCurrentPlayer() {}
-	public ArrayList<Player> getAllPlayer() {}
-	public ArrayList<Building> getAllBuilding() {}
-	public ArrayList<Map> getAllMap() {}
+	public void removeEntity(Entity entity) {}
 	
+	public void bribe(Entity target) {}
+	
+	public boolean canBribe(Entity target) {
+		return false;
+	}
+	
+	public GamePlay getInstance() {
+		if(this.instance == null) {
+			this.instance = new GamePlay();
+		}
+		return this.instance;
+	}
+	
+	public GamePlay getInstance(int roundAmount) {
+		if(this.instance == null) {
+			this.instance = new GamePlay(roundAmount);
+		}
+		return this.instance;
+	}
+	
+	public ArrayList<Player> getResult(){
+		return new ArrayList<Player>();
+	}
+
+	public int getRoundAmount() {
+		return roundAmount;
+	}
+
+	public int getCurrentRound() {
+		return currentRound;
+	}
+
+	public int getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+	public Marketplace getMarketplace() {
+		return marketplace;
+	}
+
+	public ArrayList<Player> getAllPlayers() {
+		return allPlayers;
+	}
+
+	public ArrayList<Node> getAllNodes() {
+		return allNodes;
+	}
+
+	public ArrayList<Edge> getAllEdges() {
+		return allEdges;
+	}
+
+	public ArrayList<Map> getAllMaps() {
+		return allMaps;
+	}
+
+	public ArrayList<Entity> getAllEntities() {
+		return allEntities;
+	}
+	
+	 
 }
