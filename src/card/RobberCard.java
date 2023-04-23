30fdbb70da0c8ed5e9e161d5f9e08475529cdef6
Sarin Yongsuwan <yongsuwan.sb@gmail.com>
2023-04-23 10:55:44 +0700

    Modify Class ThiefCard

--- src/card/RobberCard.java
+++ src/card/RobberCard.java
@@ -1,5 +1,24 @@
 package card;
 
-public class RobberCard {
+import buildings.Place;
+import type.CardType;
+
+public class RobberCard extends EffectCard {
+	
+	public RobberCard() {
+		super(CardType.ROBBER);
+	}
+
+	@Override
+	public boolean canPlay(Place place) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void play(Place place) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/card/ThiefCard.java
+++ src/card/ThiefCard.java
@@ -1,5 +1,43 @@
 package card;
 
-public class ThiefCard {
+import java.util.ArrayList;
+
+import buildings.Node;
+import buildings.Place;
+import entities.Entity;
+import logic.GamePlay;
+import logic.Player;
+import material.Material;
+import type.CardType;
+import type.EntityType;
+import type.MaterialType;
+
+public class ThiefCard extends EffectCard {
+
+	public ThiefCard() {
+		super(CardType.THIEF);
+	}
+
+	@Override
+	public boolean canPlay(Place place) {
+		GamePlay gameInstance = GamePlay.getInstance();
+		Player currentPlayer = gameInstance.getAllPlayers().get(gameInstance.getCurrentPlayer());
+		if (place instanceof Node && ((Node) (place)).getOwner().equals(currentPlayer)
+				&& currentPlayer.countMaterial(new Material(MaterialType.GUNPOWDER)) >= 1
+				&& currentPlayer.countMaterial(new Material(MaterialType.ROCK)) >= 1
+				&& currentPlayer.countMaterial(new Material(MaterialType.WATER)) >= 1) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void play(Place place) {
+		GamePlay gameInstance = GamePlay.getInstance();
+		Player currentPlayer = gameInstance.getAllPlayers().get(gameInstance.getCurrentPlayer());
+		Entity newEntity = new Entity(EntityType.THIEF, currentPlayer, 3, new ArrayList<Material>());
+		newEntity.setPosition((Node)place);
+		gameInstance.addEntity(newEntity);
+	}
 
 }
