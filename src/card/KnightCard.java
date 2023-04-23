be73eba13d27ae8efab55fa42e77e66ebe6c4052
Sarin Yongsuwan <yongsuwan.sb@gmail.com>
2023-04-23 10:50:23 +0700

    Modify Class CardKnight

--- src/card/KnightCard.java
+++ src/card/KnightCard.java
@@ -1,7 +1,16 @@
 package card;
 
+import java.util.ArrayList;
+
+import buildings.Node;
 import buildings.Place;
+import entities.Entity;
+import logic.GamePlay;
+import logic.Player;
+import material.Material;
 import type.CardType;
+import type.EntityType;
+import type.MaterialType;
 
 public class KnightCard extends EffectCard {
 
@@ -11,15 +20,24 @@ public class KnightCard extends EffectCard {
 
 	@Override
 	public boolean canPlay(Place place) {
-		// TODO Auto-generated method stub
+		GamePlay gameInstance = GamePlay.getInstance();
+		Player currentPlayer = gameInstance.getAllPlayers().get(gameInstance.getCurrentPlayer());
+		if (place instanceof Node && ((Node) (place)).getOwner().equals(currentPlayer)
+				&& currentPlayer.countMaterial(new Material(MaterialType.WOOD)) >= 1
+				&& currentPlayer.countMaterial(new Material(MaterialType.ROCK)) >= 1
+				&& currentPlayer.countMaterial(new Material(MaterialType.WATER)) >= 1) {
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public void play(Place place) {
-		// TODO Auto-generated method stub
-		
+		GamePlay gameInstance = GamePlay.getInstance();
+		Player currentPlayer = gameInstance.getAllPlayers().get(gameInstance.getCurrentPlayer());
+		Entity newEntity = new Entity(EntityType.KNIGHT, currentPlayer, 3, new ArrayList<Material>());
+		newEntity.setPosition((Node)place);
+		gameInstance.addEntity(newEntity);
 	}
-
 
 }
