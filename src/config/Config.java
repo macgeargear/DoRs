09865e1b3e3b94b994b81f5478386b91056cb2ac
Sarin Yongsuwan <yongsuwan.sb@gmail.com>
2023-05-21 12:23:21 +0700

    change max round to 10

--- src/config/Config.java
+++ src/config/Config.java
@@ -132,7 +132,7 @@ public class Config {
 	public static final int SCORE_3 = 5;
 	public static final int SCORE_LONGEST_PATH = 7;
 
-	public static final int MAX_ROUND = 1;
+	public static final int MAX_ROUND = 10;
 
 //	Marketplace
 	public static final int TRADE_AMOUNT = 20;
